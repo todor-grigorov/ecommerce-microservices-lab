c0b8cefce2b38f629e701e11cf9f45f0245ede17
todor-grigorov <t.grigorov99@gmail.com>
2025-12-01 00:32:09 +0100

    feat: add migration to seed db

--- ECommerce/ECommerce.Services.CouponAPI/Migrations/20251130232832_SeedCouponTables.Designer.cs
+++ ECommerce/ECommerce.Services.CouponAPI/Migrations/20251130232832_SeedCouponTables.Designer.cs
@@ -2,6 +2,7 @@
 using ECommerce.Services.CouponAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ECommerce.Services.CouponAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251130232832_SeedCouponTables")]
+    partial class SeedCouponTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
