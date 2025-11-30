8bd69bc09a4a6c4f45ec4b155b1fff54fbbd3711
todor-grigorov <t.grigorov99@gmail.com>
2025-11-30 23:32:23 +0100

    feat: add Coupon model and dto

---
+++ ECommerce/ECommerce.Services.CouponAPI/Dto/CouponDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.Services.CouponAPI.Dto
+{
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
