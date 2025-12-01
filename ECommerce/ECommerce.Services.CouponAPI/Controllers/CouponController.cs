b6edfcbf8c65856525c0efb5912a3caf599ef7b3
todor-grigorov <t.grigorov99@gmail.com>
2025-12-02 00:46:01 +0100

    feat: add MVC project

--- ECommerce/ECommerce.Services.CouponAPI/Controllers/CouponController.cs
+++ ECommerce/ECommerce.Services.CouponAPI/Controllers/CouponController.cs
@@ -1,4 +1,5 @@
-﻿using ECommerce.Services.CouponAPI.Data;
+﻿using AutoMapper;
+using ECommerce.Services.CouponAPI.Data;
 using ECommerce.Services.CouponAPI.Dto;
 using ECommerce.Services.CouponAPI.Models;
 using Microsoft.AspNetCore.Http;
@@ -11,11 +12,13 @@ namespace ECommerce.Services.CouponAPI.Controllers
     public class CouponController : ControllerBase
     {
         private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
         private ResponseDto _response;
 
-        public CouponController(AppDbContext context)
+        public CouponController(AppDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
             _response = new ResponseDto();
         }
 
@@ -25,7 +28,8 @@ namespace ECommerce.Services.CouponAPI.Controllers
             try
             {
                 var coupons = _context.Coupons.ToList();
-                _response.Result = coupons;
+                var couponsDto = _mapper.Map<List<CouponDto>>(coupons);
+                _response.Result = couponsDto;
 
             }
             catch (Exception ex)
@@ -40,12 +44,95 @@ namespace ECommerce.Services.CouponAPI.Controllers
 
         [HttpGet]
         [Route("{id:int}")]
-        public IActionResult GetCouponById(int id)
+        public IActionResult GetById(int id)
         {
             try
             {
                 var coupon = _context.Coupons.First(c => c.CouponId == id);
-                _response.Result = coupon;
+                var couponDto = _mapper.Map<CouponDto>(coupon);
+                _response.Result = couponDto;
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response.IsSuccess ? Ok(_response) : BadRequest(_response);
+        }
+
+        [HttpGet]
+        [Route("GetByCode/{code}")]
+        public IActionResult GetByCode(string code)
+        {
+            try
+            {
+                var coupon = _context.Coupons.FirstOrDefault(c => c.CouponCode.ToLower() == code.ToLower());
+                var couponDto = _mapper.Map<CouponDto>(coupon);
+                _response.Result = couponDto;
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response.IsSuccess ? Ok(_response) : BadRequest(_response);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCoupon([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                var coupon = _mapper.Map<Coupon>(couponDto);
+                _context.Coupons.Add(coupon);
+                _context.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response.IsSuccess ? Ok(_response) : BadRequest(_response);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCoupon([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                var coupon = _mapper.Map<Coupon>(couponDto);
+                _context.Coupons.Update(coupon);
+                _context.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response.IsSuccess ? Ok(_response) : BadRequest(_response);
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public IActionResult DeleteCoupon(int id)
+        {
+            try
+            {
+                var coupon = _context.Coupons.First(c => c.CouponId == id);
+                _context.Coupons.Remove(coupon);
+                _context.SaveChanges();
             }
             catch (Exception ex)
             {
---
+++ ECommerce/ECommerce.Services.CouponAPI/Profiles/MappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ECommerce.Services.CouponAPI.Dto;
+using ECommerce.Services.CouponAPI.Models;
+
+namespace ECommerce.Services.CouponAPI.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Coupon, CouponDto>().ReverseMap();
+        }
+    }
+}
