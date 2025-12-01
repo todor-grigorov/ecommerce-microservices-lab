4c66009e0527a9a0c5e121135e73d0e2e385d848
todor-grigorov <t.grigorov99@gmail.com>
2025-12-02 00:46:15 +0100

    feat: add response dto

---
+++ ECommerce/Ecommerce.Frontend.Mvc/Dto/ResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.Frontend.Mvc.Dto
+{
+    public record ResponseDto
+    {
+        public object? Result { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public string Message { get; set; } = "";
+    }
+}
