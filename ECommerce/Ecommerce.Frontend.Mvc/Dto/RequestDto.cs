2931d6ed09f9767eb63f6a49d04a9f40175ed15f
todor-grigorov <t.grigorov99@gmail.com>
2025-12-02 00:46:31 +0100

    feat: add request dto in mvc project

---
+++ ECommerce/Ecommerce.Frontend.Mvc/Dto/RequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Ecommerce.Frontend.Mvc.Dto
+{
+    public record RequestDto
+    {
+        public string ApiType { get; set; } = "GET";
+        public string Url { get; set; }
+        public object? Data { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
