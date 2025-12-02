1f7593250964ef1cdfc69089177f8d9f9f47677e
todor-grigorov <t.grigorov99@gmail.com>
2025-12-02 23:52:54 +0100

    feat: add IBaseService in mvc project

---
+++ ECommerce/Ecommerce.Frontend.Mvc/Service/IService/IBaseService.cs
@@ -0,0 +1,9 @@
+﻿using Ecommerce.Frontend.Mvc.Dto;
+
+namespace Ecommerce.Frontend.Mvc.Service.IService
+{
+    public interface IBaseService
+    {
+       Task<ResponseDto?> SendAsync(RequestDto requestDto);
+    }
+}
