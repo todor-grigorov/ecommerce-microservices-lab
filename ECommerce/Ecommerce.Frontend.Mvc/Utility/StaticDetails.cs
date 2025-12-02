50f169b04dd3c997a041d1f6f36d780a45d9b592
todor-grigorov <t.grigorov99@gmail.com>
2025-12-02 23:52:24 +0100

    feat: add ApiType enum

---
+++ ECommerce/Ecommerce.Frontend.Mvc/Utility/StaticDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace Ecommerce.Frontend.Mvc.Utility
+{
+    public class StaticDetails
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
