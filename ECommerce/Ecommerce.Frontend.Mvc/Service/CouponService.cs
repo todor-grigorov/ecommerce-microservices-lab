ac9f55c2fd6a7ff36aeaee5fc23cea7c2ba449b0
todor-grigorov <t.grigorov99@gmail.com>
2025-12-03 20:07:00 +0100

    feat: add Coupon service

---
+++ ECommerce/Ecommerce.Frontend.Mvc/Service/CouponService.cs
@@ -0,0 +1,45 @@
+﻿using Ecommerce.Frontend.Mvc.Dto;
+using Ecommerce.Frontend.Mvc.Service.IService;
+
+namespace Ecommerce.Frontend.Mvc.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IBaseService _baseService;
+
+        public CouponService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public Task<ResponseDto?> CreateCouponAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> DeleteCouponAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetAllCouponsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetCouponAsync(string couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetCouponByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> UpdateCouponAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
