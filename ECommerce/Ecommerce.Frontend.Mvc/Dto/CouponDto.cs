e4c201608d9cfd696e182c410585da71feae11b6
todor-grigorov <t.grigorov99@gmail.com>
2025-12-03 19:55:53 +0100

    feat: add Couupon dto in mvc project

---
+++ ECommerce/Ecommerce.Frontend.Mvc/Dto/CouponDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Ecommerce.Frontend.Mvc.Dto
+{
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
