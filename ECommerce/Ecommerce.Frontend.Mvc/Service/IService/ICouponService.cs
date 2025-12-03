680836f94a4dcd00c8754bf7f7759ab619765573
todor-grigorov <t.grigorov99@gmail.com>
2025-12-03 19:56:17 +0100

    feat: add ICouponService interface in MVC project

---
+++ ECommerce/Ecommerce.Frontend.Mvc/Service/IService/ICouponService.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Frontend.Mvc.Dto;
+
+namespace Ecommerce.Frontend.Mvc.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<ResponseDto?> GetAllCouponsAsync();
+        Task<ResponseDto?> GetCouponByIdAsync(int id);
+        Task<ResponseDto?> GetCouponAsync(string couponCode);
+        Task<ResponseDto?> CreateCouponAsync(CouponDto couponDto);
+        Task<ResponseDto?> UpdateCouponAsync(CouponDto couponDto);
+        Task<ResponseDto?> DeleteCouponAsync(int id);
+    }
+}
