794b5d20d1ebb225f559c31fec4519a048d08358
todor-grigorov <t.grigorov99@gmail.com>
2025-12-04 20:27:20 +0100

    feat: add Coupon MVC Controller with CouponIndex

---
+++ ECommerce/Ecommerce.Frontend.Mvc/Controllers/CouponController.cs
@@ -0,0 +1,31 @@
+﻿using Ecommerce.Frontend.Mvc.Dto;
+using Ecommerce.Frontend.Mvc.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Ecommerce.Frontend.Mvc.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto>? couponDtos = new();
+
+            ResponseDto? response = await _couponService.GetAllCouponsAsync();
+
+            if (response != null && response.IsSuccess && response.Result != null)
+            {
+                couponDtos = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+            }
+
+            return View(couponDtos );
+        }
+    }
+}
