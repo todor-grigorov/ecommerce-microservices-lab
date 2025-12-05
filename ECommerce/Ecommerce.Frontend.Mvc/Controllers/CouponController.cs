55e9e3812e0505c07b6ecb60a07024cd9bd2f763
todor-grigorov <t.grigorov99@gmail.com>
2025-12-05 21:45:48 +0100

    feat add CouponCreate and CouponDelete in CouponController (mvc project)

--- ECommerce/Ecommerce.Frontend.Mvc/Controllers/CouponController.cs
+++ ECommerce/Ecommerce.Frontend.Mvc/Controllers/CouponController.cs
@@ -2,6 +2,7 @@
 using Ecommerce.Frontend.Mvc.Service.IService;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Reflection;
 
 namespace Ecommerce.Frontend.Mvc.Controllers
 {
@@ -25,7 +26,57 @@ namespace Ecommerce.Frontend.Mvc.Controllers
                 couponDtos = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
             }
 
-            return View(couponDtos );
+            return View(couponDtos);
         }
+
+        public async Task<IActionResult> CouponCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto? response = await _couponService.CreateCouponAsync(model);
+
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> CouponDelete(int couponId)
+        {
+            ResponseDto? response = await _couponService.GetCouponByIdAsync(couponId);
+
+            if (response != null && response.IsSuccess && response.Result != null)
+            {
+                var couponDto = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(response.Result));
+
+                return View(couponDto);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponDelete(CouponDto couponDto)
+        {
+            ResponseDto? response = await _couponService.DeleteCouponAsync(couponDto.CouponId);
+
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(CouponIndex));
+            }
+
+            return View(couponDto);
+        }
+
     }
 }
