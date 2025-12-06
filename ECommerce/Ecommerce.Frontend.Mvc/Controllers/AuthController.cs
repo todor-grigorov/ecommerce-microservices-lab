47f8703a7c20a55fe180f305ff810ec0b4a724f0
todor-grigorov <t.grigorov99@gmail.com>
2025-12-07 00:11:36 +0100

    feat: add AuthController with Register functionality - MVC project

---
+++ ECommerce/Ecommerce.Frontend.Mvc/Controllers/AuthController.cs
@@ -0,0 +1,89 @@
+﻿using Ecommerce.Frontend.Mvc.Dto;
+using Ecommerce.Frontend.Mvc.Service.IService;
+using Ecommerce.Frontend.Mvc.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Ecommerce.Frontend.Mvc.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new LoginRequestDto();
+
+            return View(loginRequestDto);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem { Text = StaticDetails.RoleAdmin, Value = StaticDetails.RoleAdmin },
+                new SelectListItem { Text = StaticDetails.RoleCustomer, Value = StaticDetails.RoleCustomer },
+            };
+
+            ViewBag.RoleList = roleList;
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationRequestDto obj)
+        {
+            ResponseDto result = await _authService.RegisterAsync(obj);
+            ResponseDto assignRole;
+
+            if (result != null && result.IsSuccess)
+            {
+                if (obj.Role == StaticDetails.RoleAdmin)
+                {
+                    assignRole = await _authService.AssignRoleAsync(new AssignRoleRequestDto
+                    {
+                        Email = obj.Email,
+                        Role = StaticDetails.RoleAdmin
+                    });
+                }
+                else
+                {
+                    assignRole = await _authService.AssignRoleAsync(new AssignRoleRequestDto
+                    {
+                        Email = obj.Email,
+                        Role = StaticDetails.RoleCustomer
+                    });
+                }
+
+                if (assignRole != null && assignRole.IsSuccess)
+                {
+                    TempData["success"] = "Registration Successful";
+
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem { Text = StaticDetails.RoleAdmin, Value = StaticDetails.RoleAdmin },
+                new SelectListItem { Text = StaticDetails.RoleCustomer, Value = StaticDetails.RoleCustomer },
+            };
+
+            ViewBag.RoleList = roleList;
+
+            return View(obj);
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
