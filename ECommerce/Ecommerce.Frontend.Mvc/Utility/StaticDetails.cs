9b79b6f84ad9b8fccb5f061c7438559ebcda80a0
todor-grigorov <t.grigorov99@gmail.com>
2025-12-07 00:10:35 +0100

    feat: add RoleAdmin and RoleCustomer constants in StaticDetails

--- ECommerce/Ecommerce.Frontend.Mvc/Utility/StaticDetails.cs
+++ ECommerce/Ecommerce.Frontend.Mvc/Utility/StaticDetails.cs
@@ -4,6 +4,8 @@
     {
         public static string CouponApiBase { get; set; }
         public static string IdentityApiBase { get; set; }
+        public const string RoleAdmin = "ADMIN";
+        public const string RoleCustomer = "CUSTOMER";
         public enum ApiType
         {
             GET,
