b3f78b9298a23dfc70446ef818f070a5d3f88cb3
todor-grigorov <t.grigorov99@gmail.com>
2025-12-07 00:07:07 +0100

    feat: implment AuthService in MVC project

---
+++ ECommerce/Ecommerce.Frontend.Mvc/Service/AuthService.cs
@@ -0,0 +1,46 @@
+﻿using Ecommerce.Frontend.Mvc.Dto;
+using Ecommerce.Frontend.Mvc.Service.IService;
+using Ecommerce.Frontend.Mvc.Utility;
+
+namespace Ecommerce.Frontend.Mvc.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IBaseService _baseService;
+
+        public AuthService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public async Task<ResponseDto?> AssignRoleAsync(AssignRoleRequestDto assignRoleRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = StaticDetails.ApiType.POST,
+                Data = assignRoleRequestDto,
+                Url = StaticDetails.IdentityApiBase + "/api/auth/assign-role"
+            });
+        }
+
+        public async Task<ResponseDto?> LoginAsync(LoginRequestDto loginRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = StaticDetails.ApiType.POST,
+                Data = loginRequestDto,
+                Url = StaticDetails.IdentityApiBase + "/api/auth/login"
+            });
+        }
+
+        public async Task<ResponseDto?> RegisterAsync(RegistrationRequestDto registrationRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = StaticDetails.ApiType.POST,
+                Data = registrationRequestDto,
+                Url = StaticDetails.IdentityApiBase + "/api/auth/register"
+            });
+        }
+    }
+}
