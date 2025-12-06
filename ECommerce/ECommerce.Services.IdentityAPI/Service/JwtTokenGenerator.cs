4a061fc01243631ce7c5b85427e6c3fbc807ad04
todor-grigorov <t.grigorov99@gmail.com>
2025-12-06 20:49:10 +0100

    fix: inject IOptions<JwtOptions> the correct way

--- ECommerce/ECommerce.Services.IdentityAPI/Service/JwtTokenGenerator.cs
+++ ECommerce/ECommerce.Services.IdentityAPI/Service/JwtTokenGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using ECommerce.Services.IdentityAPI.Models;
 using ECommerce.Services.IdentityAPI.Service.IService;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -10,9 +11,9 @@ namespace ECommerce.Services.IdentityAPI.Service
     public class JwtTokenGenerator : IJwtTokenGenerator
     {
         private readonly JwtOptions _jwtOptions;
-        public JwtTokenGenerator(JwtOptions jwtOptions)
+        public JwtTokenGenerator(IOptions<JwtOptions> jwtOptions)
         {
-            _jwtOptions = jwtOptions;
+            _jwtOptions = jwtOptions.Value;
         }
 
         public string GenerateToken(ApplicationUser user)
