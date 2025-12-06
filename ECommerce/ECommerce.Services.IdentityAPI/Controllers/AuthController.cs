d4458b8a5f507778713f86ad59b7d47e1a606286
todor-grigorov <t.grigorov99@gmail.com>
2025-12-06 16:42:10 +0100

    feat: add AuthController

---
+++ ECommerce/ECommerce.Services.IdentityAPI/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Services.IdentityAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register()
+        {
+            // Registration logic goes here
+            return Ok(new { Message = "User registered successfully." });
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login()
+        {
+            // Login logic goes here
+            return Ok();
+
+        }
+
+    }
+}
