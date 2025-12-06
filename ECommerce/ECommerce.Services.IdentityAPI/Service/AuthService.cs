2a23465cc90e67b275d526517e4e0090ae0336fa
todor-grigorov <t.grigorov99@gmail.com>
2025-12-06 20:15:23 +0100

    feat: implement Register in AuthService

---
+++ ECommerce/ECommerce.Services.IdentityAPI/Service/AuthService.cs
@@ -0,0 +1,70 @@
+﻿using ECommerce.Services.IdentityAPI.Data;
+using ECommerce.Services.IdentityAPI.Dto;
+using ECommerce.Services.IdentityAPI.Models;
+using ECommerce.Services.IdentityAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
+
+namespace ECommerce.Services.IdentityAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(AppDbContext appDbContext, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _appDbContext = appDbContext;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public Task<LoginRequestDto> Login(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            ApplicationUser user = new ApplicationUser()
+            {
+                UserName = registrationRequestDto.Email,
+                Email = registrationRequestDto.Email,
+                NormalizedEmail = registrationRequestDto.Email.ToUpper(),
+                Name = registrationRequestDto.Name,
+                PhoneNumber = registrationRequestDto.PhoneNumber
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(user, registrationRequestDto.Password);
+
+                if (result.Succeeded)
+                {
+                    var userToReturn = _appDbContext.ApplicationUsers.First(u => user.UserName == registrationRequestDto.Email);
+
+                    UserDto userDto = new UserDto()
+                    {
+                        Email = userToReturn.Email,
+                        ID = userToReturn.Id,
+                        Name = userToReturn.Name,
+                        PhoneNumber = userToReturn.PhoneNumber
+                    };
+
+
+                    return "";
+                }
+                else
+                {
+                    return result.Errors.FirstOrDefault().Description;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return "Error Encountered";
+        }
+    }
+}
