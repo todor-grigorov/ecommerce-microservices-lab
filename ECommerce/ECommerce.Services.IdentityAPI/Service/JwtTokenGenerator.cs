fa52ce0a9b2baf6d8df1fd071226644964ea1e24
todor-grigorov <t.grigorov99@gmail.com>
2025-12-06 20:37:24 +0100

    feat: implement JwtTokenGenerator

---
+++ ECommerce/ECommerce.Services.IdentityAPI/Service/JwtTokenGenerator.cs
@@ -0,0 +1,45 @@
+﻿using ECommerce.Services.IdentityAPI.Models;
+using ECommerce.Services.IdentityAPI.Service.IService;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ECommerce.Services.IdentityAPI.Service
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly JwtOptions _jwtOptions;
+        public JwtTokenGenerator(JwtOptions jwtOptions)
+        {
+            _jwtOptions = jwtOptions;
+        }
+
+        public string GenerateToken(ApplicationUser user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(_jwtOptions.SecretKey);
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Name, user.UserName),
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7),
+                Issuer = _jwtOptions.Issuer,
+                Audience = _jwtOptions.Audience,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
