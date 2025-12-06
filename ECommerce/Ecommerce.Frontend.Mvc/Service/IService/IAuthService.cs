2f42343d875bbfc50240fef28067be840f948e7f
todor-grigorov <t.grigorov99@gmail.com>
2025-12-07 00:06:46 +0100

    feat: add IAuthService in MVC project

---
+++ ECommerce/Ecommerce.Frontend.Mvc/Service/IService/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Ecommerce.Frontend.Mvc.Dto;
+
+namespace Ecommerce.Frontend.Mvc.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<ResponseDto?> LoginAsync(LoginRequestDto loginRequestDto);
+        Task<ResponseDto?> RegisterAsync(RegistrationRequestDto registrationRequestDto);
+        Task<ResponseDto?> AssignRoleAsync(AssignRoleRequestDto assignRoleRequestDto);
+    }
+}
