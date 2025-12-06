8f6c93a7ce9aa3f42ec3ec6e809cb829aa86d0e7
todor-grigorov <t.grigorov99@gmail.com>
2025-12-06 20:14:59 +0100

    feat: add IAuthService

---
+++ ECommerce/ECommerce.Services.IdentityAPI/Service/IService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using ECommerce.Services.IdentityAPI.Dto;
+
+namespace ECommerce.Services.IdentityAPI.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<string> Register(RegistrationRequestDto registrationRequestDto);
+        Task<LoginRequestDto> Login(LoginRequestDto loginRequestDto);
+    }
+}
