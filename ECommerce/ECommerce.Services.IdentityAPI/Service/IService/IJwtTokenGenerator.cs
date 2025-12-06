dd482175a24e43c26675363313c3e61a26331bee
todor-grigorov <t.grigorov99@gmail.com>
2025-12-06 20:37:09 +0100

    feat: add IJwtTokenGenerator

---
+++ ECommerce/ECommerce.Services.IdentityAPI/Service/IService/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using ECommerce.Services.IdentityAPI.Models;
+
+namespace ECommerce.Services.IdentityAPI.Service.IService
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(ApplicationUser user);
+    }
+}
