1b7d0d7d6eec8cca520a753aa7765fb245fe5f53
todor-grigorov <t.grigorov99@gmail.com>
2025-12-06 13:07:08 +0100

    feat: add ApplicationUser which extends the default IdentityUser

---
+++ ECommerce/ECommerce.Services.IdentityAPI/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ECommerce.Services.IdentityAPI.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+    }
+}
