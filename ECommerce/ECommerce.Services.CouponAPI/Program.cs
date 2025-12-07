f02a0cf43a8a607b43552250e56236c4c45c8b0a
todor-grigorov <t.grigorov99@gmail.com>
2025-12-07 20:11:21 +0100

    feat: add Authentication and  Authorization for Swagger in CouponAPI

--- ECommerce/ECommerce.Services.CouponAPI/Program.cs
+++ ECommerce/ECommerce.Services.CouponAPI/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,11 +17,37 @@ builder.Services.AddDbContext<AppDbContext>(opts =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme, securityScheme: new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\""
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = JwtBearerDefaults.AuthenticationScheme
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
-var sercret = builder.Configuration.GetValue<string>("AppSettings:Secret");
-var issuer = builder.Configuration.GetValue<string>("AppSettings:Issuer");
-var audience = builder.Configuration.GetValue<string>("AppSettings:Audience");
+var settingsSection = builder.Configuration.GetSection("ApiSettings");
+
+var sercret = settingsSection.GetValue<string>("SecretKey");
+var issuer = settingsSection.GetValue<string>("Issuer");
+var audience = settingsSection.GetValue<string>("Audience");
 
 var key = Encoding.ASCII.GetBytes(sercret);
 
