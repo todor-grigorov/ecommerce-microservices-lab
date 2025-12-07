969eb1047739f47324b955e35dd5e84692540f09
todor-grigorov <t.grigorov99@gmail.com>
2025-12-07 20:20:17 +0100

    feat: add token to IBaseService and BaseService in MVC project

--- ECommerce/Ecommerce.Frontend.Mvc/Service/BaseService.cs
+++ ECommerce/Ecommerce.Frontend.Mvc/Service/BaseService.cs
@@ -10,12 +10,15 @@ namespace Ecommerce.Frontend.Mvc.Service
     public class BaseService : IBaseService
     {
         private readonly IHttpClientFactory _httpClientFactory;
-        public BaseService(IHttpClientFactory httpClientFactory)
+        private readonly ITokenProvider _tokenProvider;
+
+        public BaseService(IHttpClientFactory httpClientFactory, ITokenProvider tokenProvider)
         {
             _httpClientFactory = httpClientFactory;
+            _tokenProvider = tokenProvider;
         }
 
-        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto, bool withBearer = true)
         {
             try
             {
@@ -25,6 +28,11 @@ namespace Ecommerce.Frontend.Mvc.Service
                 HttpRequestMessage message = new HttpRequestMessage();
                 message.Headers.Add("Accept", "application/json");
                 //token
+                if (withBearer)
+                {
+                    var token = _tokenProvider.GetToken();
+                    message.Headers.Add("Authorization", "Bearer " + token);
+                }
 
                 message.RequestUri = new Uri(requestDto.Url);
 
--- ECommerce/Ecommerce.Frontend.Mvc/Service/IService/IBaseService.cs
+++ ECommerce/Ecommerce.Frontend.Mvc/Service/IService/IBaseService.cs
@@ -4,6 +4,6 @@ namespace Ecommerce.Frontend.Mvc.Service.IService
 {
     public interface IBaseService
     {
-       Task<ResponseDto?> SendAsync(RequestDto requestDto);
+       Task<ResponseDto?> SendAsync(RequestDto requestDto, bool withBearer = true);
     }
 }
