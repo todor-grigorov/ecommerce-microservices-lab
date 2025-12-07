1d5332864c650f0f928ae937c2b7d04e460c9c66
todor-grigorov <t.grigorov99@gmail.com>
2025-12-07 11:54:53 +0100

    feat: add ITokenProvider and TokenProvider services

---
+++ ECommerce/Ecommerce.Frontend.Mvc/Service/IService/ITokenProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.Frontend.Mvc.Service.IService
+{
+    public interface ITokenProvider
+    {
+        void SetToken(string token);
+        string? GetToken();
+        void ClearToken();
+    }
+}
---
+++ ECommerce/Ecommerce.Frontend.Mvc/Service/TokenProvider.cs
@@ -0,0 +1,35 @@
+﻿using Ecommerce.Frontend.Mvc.Service.IService;
+using Ecommerce.Frontend.Mvc.Utility;
+using Newtonsoft.Json.Linq;
+
+namespace Ecommerce.Frontend.Mvc.Service
+{
+    public class TokenProvider : ITokenProvider
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public TokenProvider(IHttpContextAccessor httpContextAccessor) 
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public void ClearToken()
+        {
+            _httpContextAccessor.HttpContext?.Response.Cookies.Delete(StaticDetails.TokenCookie);
+        }
+
+        public string? GetToken()
+        {
+            string? token = null;
+
+            bool? hasToken = _httpContextAccessor.HttpContext?.Request.Cookies.TryGetValue(StaticDetails.TokenCookie, out token);
+
+            return hasToken is true ? token : null;
+        }
+
+        public void SetToken(string token)
+        {
+            _httpContextAccessor.HttpContext?.Response.Cookies.Append(StaticDetails.TokenCookie, token);
+        }
+    }
+}
