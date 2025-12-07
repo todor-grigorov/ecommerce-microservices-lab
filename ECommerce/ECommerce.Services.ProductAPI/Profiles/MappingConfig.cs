5da5aba1d2ad3428067f5c33f13b0e8dd0117460
todor-grigorov <t.grigorov99@gmail.com>
2025-12-07 21:15:45 +0100

    feat: setup Automapper

---
+++ ECommerce/ECommerce.Services.ProductAPI/Profiles/MappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ECommerce.Services.ProductAPI.Dto;
+using ECommerce.Services.ProductAPI.Models;
+
+namespace ECommerce.Services.CouponAPI.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+        }
+    }
+}
--- ECommerce/ECommerce.Services.ProductAPI/Program.cs
+++ ECommerce/ECommerce.Services.ProductAPI/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
 // Add services to the container.
+builder.Services.AddAutoMapper(cfg => { }, typeof(Program));
 builder.Services.AddDbContext<AppDbContext>(opts =>
                 opts.UseNpgsql(configuration.GetConnectionString("postgresConnection")));
 
