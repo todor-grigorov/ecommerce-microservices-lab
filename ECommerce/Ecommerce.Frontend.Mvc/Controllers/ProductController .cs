a1d69468aaa25aeff7c9dded4880a1bf04e7bb7e
todor-grigorov <t.grigorov99@gmail.com>
2025-12-07 22:08:32 +0100

    feat: add Product Service with all methods

---
+++ ECommerce/Ecommerce.Frontend.Mvc/Controllers/ProductController .cs
@@ -0,0 +1,98 @@
+﻿using Ecommerce.Frontend.Mvc.Dto;
+using Ecommerce.Frontend.Mvc.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Ecommerce.Frontend.Mvc.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDto>? productDtos = new();
+
+            ResponseDto? response = await _productService.GetAllProductsAsync();
+
+            if (response != null && response.IsSuccess && response.Result != null)
+            {
+                productDtos = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+
+            return View(productDtos);
+        }
+
+        public async Task<IActionResult> ProductCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductCreate(ProductDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto? response = await _productService.CreateProductAsync(model);
+
+                if (response != null && response.IsSuccess)
+                {
+                    TempData["success"] = "Product created successfully.";
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+                else
+                {
+                    TempData["error"] = response?.Message;
+                }
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductDelete(int productId)
+        {
+            ResponseDto? response = await _productService.GetProductByIdAsync(productId);
+
+            if (response != null && response.IsSuccess && response.Result != null)
+            {
+                var productDto = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+
+                return View(productDto);
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductDelete(ProductDto productDto)
+        {
+            ResponseDto? response = await _productService.DeleteProductAsync(productDto.ProductId);
+
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Product deleted successfully.";
+                return RedirectToAction(nameof(ProductIndex));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+
+            return View(productDto);
+        }
+
+    }
+}
