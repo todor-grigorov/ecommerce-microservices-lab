d353f800a2bbad8b4a72c9509f0e71b1c6a04511
todor-grigorov <t.grigorov99@gmail.com>
2025-12-09 20:34:03 +0100

    feat: update Product Dto in MVC project

--- ECommerce/Ecommerce.Frontend.Mvc/Dto/ProductDto.cs
+++ ECommerce/Ecommerce.Frontend.Mvc/Dto/ProductDto.cs
@@ -1,4 +1,6 @@
-﻿namespace Ecommerce.Frontend.Mvc.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Frontend.Mvc.Dto
 {
     public class ProductDto
     {
@@ -8,5 +10,8 @@
         public string Description { get; set; }
         public string CategoryName { get; set; }
         public string ImageUrl { get; set; }
+
+        [Range(1, 100)]
+        public int Count { get; set; } = 1;
     }
 }
