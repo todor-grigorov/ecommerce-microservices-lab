810960fb6f819ee03f6395319eab34d2e6d8c680
todor-grigorov <t.grigorov99@gmail.com>
2025-12-10 00:44:01 +0100

    feat: add mapping profiles

---
+++ ECommerce/ECommerce.Services.ShoppingCartAPI/Profiles/MappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ECommerce.Services.ShoppingCartAPI.Dto;
+using ECommerce.Services.ShoppingCartAPI.Models;
+
+namespace ECommerce.Services.ShoppingCartAPI.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+            CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+        }
+    }
+}
