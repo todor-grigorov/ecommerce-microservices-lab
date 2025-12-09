d26f483b3831d66f03d8ddefb6e83dc0e40f5f5c
todor-grigorov <t.grigorov99@gmail.com>
2025-12-09 20:52:03 +0100

    feat: add all necessary DTOs

---
+++ ECommerce/ECommerce.Services.ShoppingCartAPI/Dto/CartDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.Services.ShoppingCartAPI.Dto
+{
+    public class CartDto
+    {
+        public CartHeaderDto CartHeader { get; set; }
+        public IEnumerable<CartDetailsDto>? CartDetails { get; set; }
+    }
+}
