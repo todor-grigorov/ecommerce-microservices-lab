edfdcdd06a4ed266f20cd0301dcce032716e0067
todor-grigorov <t.grigorov99@gmail.com>
2025-12-11 16:28:05 +0100

    feat: add product to shopping cart

--- ECommerce/Ecommerce.Frontend.Mvc/Controllers/HomeController.cs
+++ ECommerce/Ecommerce.Frontend.Mvc/Controllers/HomeController.cs
@@ -5,16 +5,19 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace ECommerce.Frontend.Mvc.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICartService _cartService;
 
-        public HomeController(ILogger<HomeController> logger, IProductService productService)
+        public HomeController(IProductService productService, ICartService cartService)
         {
             _productService = productService;
+            _cartService = cartService;
         }
 
         public async Task<IActionResult> Index()
@@ -45,6 +48,43 @@ namespace ECommerce.Frontend.Mvc.Controllers
             if (response != null && response.IsSuccess && response.Result != null)
             {
                 productDto = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+
+            return View(productDto);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ActionName("ProductDetails")]
+        public async Task<IActionResult> ProductDetails(ProductDto productDto)
+        {
+            CartDto cartDto = new()
+            {
+                CartHeader = new CartHeaderDto
+                {
+                    UserId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value
+                }
+            };
+
+            CartDetailsDto cartDetailsDto = new()
+            {
+                ProductId = productDto.ProductId,
+                Count = productDto.Count
+            };
+
+            List<CartDetailsDto> cartDetailsDtos = new() { cartDetailsDto };
+            cartDto.CartDetails = cartDetailsDtos;
+
+            ResponseDto? response = await _cartService.UpsertCartAsync(cartDto);
+
+            if (response != null && response.IsSuccess && response.Result != null)
+            {
+                TempData["success"] = "Item has been added to the Shopping Cart";
+                return RedirectToAction(nameof(Index));
             }
             else
             {
