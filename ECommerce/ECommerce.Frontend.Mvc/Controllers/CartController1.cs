54b6cee85e9e169ef6b51e5635a0bfb241dfe07b
todor-grigorov <t.grigorov99@gmail.com>
2025-12-11 15:29:19 +0100

    feat: add Cart controller with Index (MVC project)

---
+++ ECommerce/ECommerce.Frontend.Mvc/Controllers/CartController1.cs
@@ -0,0 +1,39 @@
+﻿using ECommerce.Frontend.Mvc.Dto;
+using ECommerce.Frontend.Mvc.Service.IService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace ECommerce.Frontend.Mvc.Controllers
+{
+    public class CartController1 : Controller
+    {
+        private readonly ICartService _cartService;
+
+        public CartController1(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> CartIndex()
+        {
+            return View();
+        }
+
+        private async Task<CartDto> LoadCartDtoBasedOnLoggedInUser()
+        {
+            var userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+            var response = await _cartService.GetCartByUserIdAsync(userId);
+            CartDto cartDto = new();
+
+            if (response != null && response.IsSuccess && response.Result != null)
+            {
+                cartDto = JsonConvert.DeserializeObject<CartDto>(Convert.ToString(response.Result))!;
+            }
+
+            return cartDto;
+        }
+    }
+}
