27e01742e17338118b54583f248c83b757e0718e
todor-grigorov <t.grigorov99@gmail.com>
2025-12-12 21:04:43 +0100

    feat: add EmailCartAsync to ICartService

--- ECommerce/ECommerce.Frontend.Mvc/Service/IService/ICartService.cs
+++ ECommerce/ECommerce.Frontend.Mvc/Service/IService/ICartService.cs
@@ -4,10 +4,11 @@ namespace ECommerce.Frontend.Mvc.Service.IService
 {
     public interface ICartService
     {
-        Task<ResponseDto> GetCartByUserIdAsync(string userId);
-        Task<ResponseDto> UpsertCartAsync(CartDto cartDto);
-        Task<ResponseDto> RemoveFromCartAsync(int cartDetailsId);
-        Task<ResponseDto> ApplyCouponAsync(CartDto cartDto);
-        Task<ResponseDto> RemoveCouponAsync(CartDto cartDto);
+        Task<ResponseDto?> GetCartByUserIdAsync(string userId);
+        Task<ResponseDto?> UpsertCartAsync(CartDto cartDto);
+        Task<ResponseDto?> RemoveFromCartAsync(int cartDetailsId);
+        Task<ResponseDto?> ApplyCouponAsync(CartDto cartDto);
+        Task<ResponseDto?> RemoveCouponAsync(CartDto cartDto);
+        Task<ResponseDto?> EmailCartAsync(CartDto cartDto);
     }
 }
