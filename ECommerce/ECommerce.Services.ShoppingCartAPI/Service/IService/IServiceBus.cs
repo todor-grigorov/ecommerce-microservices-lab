ac2ddbe8b1d3faa97350c9b70350ebc5f77646c3
todor-grigorov <t.grigorov99@gmail.com>
2025-12-12 21:02:19 +0100

    feat: add IServiceBus in ShoppingCartAPI

---
+++ ECommerce/ECommerce.Services.ShoppingCartAPI/Service/IService/IServiceBus.cs
@@ -0,0 +1,7 @@
+﻿namespace ECommerce.Services.ShoppingCartAPI.Service.IService
+{
+    public interface IServiceBus
+    {
+        Task PublishMessageAsync<T>(T message, string topic_queue_Name);
+    }
+}
