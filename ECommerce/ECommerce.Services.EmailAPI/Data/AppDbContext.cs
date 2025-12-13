174951790b2d4592582d8cc22107332aa50eadd9
todor-grigorov <t.grigorov99@gmail.com>
2025-12-13 21:23:36 +0100

    feat: add AppDbContext in EmailAPI project

---
+++ ECommerce/ECommerce.Services.EmailAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Services.EmailAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Services.EmailAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<EmailLogger> Products { get; set; }
+
+    }
+}
