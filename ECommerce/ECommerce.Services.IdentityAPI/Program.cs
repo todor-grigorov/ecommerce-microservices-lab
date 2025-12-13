73e486db928fb2a899a48fa00b707d86f2798f20
todor-grigorov <t.grigorov99@gmail.com>
2025-12-13 20:50:19 +0100

    feat: use top level statement in the Identity API project

--- ECommerce/ECommerce.Services.IdentityAPI/Program.cs
+++ ECommerce/ECommerce.Services.IdentityAPI/Program.cs
@@ -5,62 +5,52 @@ using ECommerce.Services.IdentityAPI.Service;
 using ECommerce.Services.IdentityAPI.Service.IService;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
-namespace ECommerce.Services.IdentityAPI
+var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opts =>
+    opts.UseNpgsql(configuration.GetConnectionString("postgresConnection")));
+
+builder.Services.Configure<JwtOptions>(configuration.GetSection("ApiSettings:JwtOptions"));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+builder.Services.AddControllers();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapControllers();
+ApplyMigrations(app);
+app.Run();
+
+void ApplyMigrations(IHost app)
+{
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    if (dbContext.Database.GetPendingMigrations().Any())
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            var configuration = builder.Configuration;
-
-            // Add services to the container.
-            builder.Services.AddDbContext<AppDbContext>(opts =>
-                opts.UseNpgsql(configuration.GetConnectionString("postgresConnection")));
-
-            builder.Services.Configure<JwtOptions>(configuration.GetSection("ApiSettings:JwtOptions"));
-
-            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<AppDbContext>()
-                .AddDefaultTokenProviders();
-            builder.Services.AddControllers();
-            builder.Services.AddScoped<IAuthService, AuthService>();
-            builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-            ApplyMigrations(app);
-            app.Run();
-
-            void ApplyMigrations(IHost app)
-            {
-                using var scope = app.Services.CreateScope();
-                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-
-                if (dbContext.Database.GetPendingMigrations().Any())
-                {
-                    dbContext.Database.Migrate();
-                }
-            }
-        }
+        dbContext.Database.Migrate();
     }
 }
