a9a734115d15a1acf073ffbe0c24ccb0394c5815
todor-grigorov <t.grigorov99@gmail.com>
2025-12-13 21:24:28 +0100

    feat: add necessary DTOs in EmailAPI project

---
+++ ECommerce/ECommerce.Services.EmailAPI/Dto/CartDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.Services.EmailAPI.Dto
+{
+    public class CartDto
+    {
+        public CartHeaderDto CartHeader { get; set; }
+        public IEnumerable<CartDetailsDto>? CartDetails { get; set; }
+    }
+}
