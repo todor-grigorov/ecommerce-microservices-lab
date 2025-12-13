b07d86908be3d4ff152a7a2515a836c0f7cc69d9
todor-grigorov <t.grigorov99@gmail.com>
2025-12-13 21:24:00 +0100

    feat: setup Program.cs in EmailAPI project

--- ECommerce/ECommerce.Services.EmailAPI/Program.cs
+++ ECommerce/ECommerce.Services.EmailAPI/Program.cs
@@ -1,4 +1,13 @@
+using ECommerce.Services.EmailAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opts =>
+                opts.UseNpgsql(configuration.GetConnectionString("postgresConnection")));
 
 // Add services to the container.
 
@@ -22,4 +31,18 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+ApplyMigrations(app);
+
 app.Run();
+
+void ApplyMigrations(IHost app)
+{
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    if (dbContext.Database.GetPendingMigrations().Any())
+    {
+        dbContext.Database.Migrate();
+    }
+}
+
