f388db8b029bf3f6af03475ca3f0fb8a8a77fa9c
todor-grigorov <t.grigorov99@gmail.com>
2025-12-13 22:35:16 +0100

    feat: add/implement IAzureServiceBusConsumer and AzureServiceBusConsumer in EmailAPI

---
+++ ECommerce/ECommerce.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,65 @@
+﻿using Azure.Messaging.ServiceBus;
+using ECommerce.Services.EmailAPI.Dto;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace ECommerce.Services.EmailAPI.Messaging
+{
+    public class AzureServiceBusConsumer : IAzureServiceBusConsumer
+    {
+        private readonly string serviceBusConnectionString;
+        private readonly string emailCartQueue;
+        private readonly IConfiguration _configuration;
+
+        private readonly ServiceBusProcessor _processor;
+
+        public AzureServiceBusConsumer(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            serviceBusConnectionString = configuration.GetConnectionString("ServiceBusConnection");
+            emailCartQueue = configuration.GetValue<string>("TopicAndQueueNames:EmailShoppingCartQueue");
+
+            var client = new ServiceBusClient(serviceBusConnectionString);
+            _processor = client.CreateProcessor(emailCartQueue);
+        }
+
+        public async Task Start()
+        {
+            _processor.ProcessMessageAsync += OnEmailCartReceived;
+            _processor.ProcessErrorAsync += ErrorHandler;
+            await _processor.StartProcessingAsync();
+        }
+        public async Task Stop()
+        {
+            await _processor.StopProcessingAsync();
+            await _processor.DisposeAsync();
+        }
+
+
+        private async Task OnEmailCartReceived(ProcessMessageEventArgs args)
+        {
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+            CartDto cartDto = JsonConvert.DeserializeObject<CartDto>(body);
+
+            try
+            {
+                // TODO: Implement email sending logic here using cartDto
+                await args.CompleteMessageAsync(args.Message);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        private Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+
+    }
+}
