9d4fc3b2a5f3f984577242259208ff6e1b4ae793
todor-grigorov <t.grigorov99@gmail.com>
2025-12-14 13:31:07 +0100

    feat: add IEmailService in EmailAPI

---
+++ ECommerce/ECommerce.Services.EmailAPI/Service/IService/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using ECommerce.Services.EmailAPI.Dto;
+
+namespace ECommerce.Services.EmailAPI.Service.IService
+{
+    public interface IEmailService
+    {
+        Task EmailCartAndLog(CartDto cartDto);
+    }
+}
