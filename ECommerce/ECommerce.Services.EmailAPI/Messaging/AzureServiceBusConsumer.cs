e9f7a666237e5cab9b804fcfc6d673d517a0eaa6
todor-grigorov <t.grigorov99@gmail.com>
2025-12-14 22:57:54 +0100

    feat: add  registerUserQueue and _registerUserProcessor to AzureServiceBusConsumer (EmailAPI)

--- ECommerce/ECommerce.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
+++ ECommerce/ECommerce.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
@@ -1,5 +1,6 @@
 ﻿using Azure.Messaging.ServiceBus;
 using ECommerce.Services.EmailAPI.Dto;
+using ECommerce.Services.EmailAPI.Service.IService;
 using Newtonsoft.Json;
 using System.Text;
 
@@ -9,32 +10,84 @@ namespace ECommerce.Services.EmailAPI.Messaging
     {
         private readonly string serviceBusConnectionString;
         private readonly string emailCartQueue;
+        private readonly string registerUserQueue;
         private readonly IConfiguration _configuration;
+        private readonly IEmailService _emailService;
+        private ServiceBusProcessor _registerUserProcessor;
 
-        private readonly ServiceBusProcessor _processor;
+        private readonly ServiceBusProcessor _emailCartProcessor;
 
-        public AzureServiceBusConsumer(IConfiguration configuration)
+        public AzureServiceBusConsumer(IConfiguration configuration, IEmailService emailService)
         {
+            _emailService = emailService;
             _configuration = configuration;
             serviceBusConnectionString = configuration.GetConnectionString("ServiceBusConnection");
             emailCartQueue = configuration.GetValue<string>("TopicAndQueueNames:EmailShoppingCartQueue");
+            registerUserQueue = configuration.GetValue<string>("TopicAndQueueNames:RegisterUserQueue");
+
 
             var client = new ServiceBusClient(serviceBusConnectionString);
-            _processor = client.CreateProcessor(emailCartQueue);
+            _emailCartProcessor = client.CreateProcessor(emailCartQueue);
+            _registerUserProcessor = client.CreateProcessor(registerUserQueue);
         }
 
         public async Task Start()
         {
-            _processor.ProcessMessageAsync += OnEmailCartReceived;
-            _processor.ProcessErrorAsync += ErrorHandler;
-            await _processor.StartProcessingAsync();
+            await StartEmailCartProcessor();
+            await StartRegisterUserProcessor();
+
         }
         public async Task Stop()
         {
-            await _processor.StopProcessingAsync();
-            await _processor.DisposeAsync();
+            await StopEmailCartProcessor();
+            await StopRegisterUserProcessor();
         }
 
+
+        private async Task StartEmailCartProcessor()
+        {
+            _emailCartProcessor.ProcessMessageAsync += OnEmailCartReceived;
+            _emailCartProcessor.ProcessErrorAsync += ErrorHandler;
+            await _emailCartProcessor.StartProcessingAsync();
+        }
+
+        private async Task StopEmailCartProcessor()
+        {
+            await _emailCartProcessor.StopProcessingAsync();
+            await _emailCartProcessor.DisposeAsync();
+        }
+
+        private async Task StartRegisterUserProcessor()
+        {
+            _registerUserProcessor.ProcessMessageAsync += OnRegisterUserReceived;
+            _registerUserProcessor.ProcessErrorAsync += ErrorHandler;
+            await _registerUserProcessor.StartProcessingAsync();
+        }
+        private async Task StopRegisterUserProcessor()
+        {
+            await _registerUserProcessor.StopProcessingAsync();
+            await _registerUserProcessor.DisposeAsync();
+        }
+
+        private async Task OnRegisterUserReceived(ProcessMessageEventArgs args)
+        {
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            string userEmail = JsonConvert.DeserializeObject<string>(body);
+
+            try
+            {
+                // TODO: Implement email sending logic here using cartDto
+                await _emailService.RegisterUserEmailAndLog(userEmail);
+                await args.CompleteMessageAsync(args.Message);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
 
         private async Task OnEmailCartReceived(ProcessMessageEventArgs args)
         {
@@ -45,6 +98,7 @@ namespace ECommerce.Services.EmailAPI.Messaging
             try
             {
                 // TODO: Implement email sending logic here using cartDto
+                await _emailService.EmailCartAndLog(cartDto);
                 await args.CompleteMessageAsync(args.Message);
             }
             catch (Exception ex)
@@ -59,7 +113,5 @@ namespace ECommerce.Services.EmailAPI.Messaging
             Console.WriteLine(args.Exception.ToString());
             return Task.CompletedTask;
         }
-
-
     }
 }
