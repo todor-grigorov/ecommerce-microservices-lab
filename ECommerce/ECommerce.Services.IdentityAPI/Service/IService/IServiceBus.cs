617fb6393bafc06285902a43000d71684a60d893
todor-grigorov <t.grigorov99@gmail.com>
2025-12-14 22:58:49 +0100

    feat: add/implement  IServiceBus and ServiceBus in IdentityAPI

---
+++ ECommerce/ECommerce.Services.IdentityAPI/Service/IService/IServiceBus.cs
@@ -0,0 +1,7 @@
+﻿namespace ECommerce.Services.IdentityAPI.Service.IService
+{
+    public interface IServiceBus
+    {
+        Task PublishMessageAsync<T>(T message, string topic_queue_Name);
+    }
+}
---
+++ ECommerce/ECommerce.Services.IdentityAPI/Service/ServiceBus.cs
@@ -0,0 +1,29 @@
+﻿using ECommerce.Integration.MessageBus;
+using ECommerce.Services.IdentityAPI.Service.IService;
+
+namespace ECommerce.Services.IdentityAPI.Service
+{
+    public class ServiceBus : IServiceBus
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string? _connectionString;
+        private readonly IMessageBus _messageBus;
+
+        public ServiceBus(IConfiguration configuration, IMessageBus messageBus)
+        {
+            _configuration = configuration;
+            _messageBus = messageBus;
+            _connectionString = _configuration.GetConnectionString("ServiceBusConnection");
+        }
+
+        public async Task PublishMessageAsync<T>(T message, string topic_queue_Name)
+        {
+            if (string.IsNullOrEmpty(_connectionString))
+            {
+                throw new InvalidOperationException("Service Bus connection string is not configured.");
+            }
+
+            await _messageBus.PublishMessageAsync(_connectionString, message, topic_queue_Name);
+        }
+    }
+}
