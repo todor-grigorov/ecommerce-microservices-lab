3e2c2e7ca6f5550889917914a85a8a160c6f0b31
todor-grigorov <t.grigorov99@gmail.com>
2025-12-15 23:21:42 +0100

    feat: add other necessary DTOs in OrderAPI

---
+++ ECommerce/ECommerce.Services.OrderAPI/Dto/ResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerce.Services.OrderAPI.Dto
+{
+    public record ResponseDto
+    {
+        public object? Result { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public string Message { get; set; } = "";
+    }
+}
