24fc1159bdf773b893b07e0dcd0586c49a45a9f5
todor-grigorov <t.grigorov99@gmail.com>
2025-12-15 23:20:42 +0100

    feat: add OrderDetails model in OrderAPI

---
+++ ECommerce/ECommerce.Services.OrderAPI/Models/OrderDetails.cs
@@ -0,0 +1,21 @@
+﻿using ECommerce.Services.OrderAPI.Dto;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECommerce.Services.OrderAPI.Models
+{
+    public class OrderDetails
+    {
+        public int OrderDetailsId { get; set; }
+        public int OrderHeaderId { get; set; }
+
+        [ForeignKey("OrderHeaderId")]
+        public OrderHeader? OrderHeader { get; set; }
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; }
+
+        [NotMapped]
+        public ProductDto? Product { get; set; }
+        public double Price { get; set; }
+        public int Count { get; set; }
+    }
+}
