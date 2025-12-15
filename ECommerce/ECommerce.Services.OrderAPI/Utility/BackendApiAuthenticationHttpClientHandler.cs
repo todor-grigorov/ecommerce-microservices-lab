1f9553441d2939bf2850f0875346a1d6ce20afa6
todor-grigorov <t.grigorov99@gmail.com>
2025-12-15 23:35:44 +0100

    feat: add BackendApiAuthenticationHttpClientHandler (OrderAPI)

---
+++ ECommerce/ECommerce.Services.OrderAPI/Utility/BackendApiAuthenticationHttpClientHandler.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace ECommerce.Services.OrderAPI.Utility
+{
+    public class BackendApiAuthenticationHttpClientHandler : DelegatingHandler
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public BackendApiAuthenticationHttpClientHandler(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var accessToken = await _httpContextAccessor.HttpContext!
+                .GetTokenAsync("access_token");
+            if (!string.IsNullOrEmpty(accessToken))
+            {
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+            }
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
