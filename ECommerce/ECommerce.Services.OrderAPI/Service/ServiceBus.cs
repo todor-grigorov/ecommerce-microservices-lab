6f71e77d2179cee6b503f959881b4b2595ba306f
todor-grigorov <t.grigorov99@gmail.com>
2025-12-15 23:49:08 +0100

    fix: ServiceBus service constructop params (OrderAPI)

--- ECommerce/ECommerce.Services.OrderAPI/Service/ServiceBus.cs
+++ ECommerce/ECommerce.Services.OrderAPI/Service/ServiceBus.cs
@@ -5,17 +5,24 @@ namespace ECommerce.Services.OrderAPI.Service
 {
     public class ServiceBus : IServiceBus
     {
-        private readonly string _connectionString;
+        private readonly IConfiguration _configuration;
+        private readonly string? _connectionString;
         private readonly IMessageBus _messageBus;
 
-        public ServiceBus(string connectionString, IMessageBus messageBus)
+        public ServiceBus(IConfiguration configuration, IMessageBus messageBus)
         {
-            _connectionString = connectionString;
+            _configuration = configuration;
             _messageBus = messageBus;
+            _connectionString = _configuration.GetConnectionString("ServiceBusConnection");
         }
 
         public async Task PublishMessageAsync<T>(T message, string topic_queue_Name)
         {
+            if (string.IsNullOrEmpty(_connectionString))
+            {
+                throw new InvalidOperationException("Service Bus connection string is not configured.");
+            }
+
             await _messageBus.PublishMessageAsync(_connectionString, message, topic_queue_Name);
         }
     }
