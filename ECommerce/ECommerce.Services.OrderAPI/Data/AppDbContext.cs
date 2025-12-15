d6392f8b53d199fff1ff555b3adc6438114fb8f1
todor-grigorov <t.grigorov99@gmail.com>
2025-12-15 23:35:22 +0100

    feat: add DB context

---
+++ ECommerce/ECommerce.Services.OrderAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Services.OrderAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Services.OrderAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+    }
+}
