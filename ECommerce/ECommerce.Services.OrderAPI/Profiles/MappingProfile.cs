a2aabb14a106d5dde12df4becac4f9c0a08ef855
todor-grigorov <t.grigorov99@gmail.com>
2025-12-15 23:36:16 +0100

    feat: add empty MappingProfile (OrderAPI)

---
+++ ECommerce/ECommerce.Services.OrderAPI/Profiles/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace ECommerce.Services.OrderAPI.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+            //CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+        }
+    }
+}
