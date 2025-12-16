45261fda1a323b741d577ba41624af809e5271f8
todor-grigorov <t.grigorov99@gmail.com>
2025-12-16 13:32:44 +0100

    feat: add OrderController in the OrderAPI

---
+++ ECommerce/ECommerce.Services.OrderAPI/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using ECommerce.Services.OrderAPI.Data;
+using ECommerce.Services.OrderAPI.Dto;
+using ECommerce.Services.OrderAPI.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Services.OrderAPI.Controllers
+{
+    [Route("api/order")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IMapper _mapper;
+        private readonly AppDbContext _dbContext;
+        private readonly IProductService _productService;
+
+        public OrderController(AppDbContext dbContext, IMapper mapper, IProductService productService)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+            _productService = productService;
+            _response = new ResponseDto();
+        }
+    }
+}
