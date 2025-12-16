f564f13b4c0c28c67473db2d07dac3e40f0abac9
todor-grigorov <t.grigorov99@gmail.com>
2025-12-16 21:04:40 +0100

    feat: add IStripeService (OrderAPI)

---
+++ ECommerce/ECommerce.Services.OrderAPI/Service/IService/IStripeService.cs
@@ -0,0 +1,10 @@
+﻿using ECommerce.Services.OrderAPI.Dto;
+using Stripe.Checkout;
+
+namespace ECommerce.Services.OrderAPI.Service.IService
+{
+    public interface IStripeService
+    {
+        Session? CreateStripeSession(StripeRequestDto stripeRequestDto);
+    }
+}
