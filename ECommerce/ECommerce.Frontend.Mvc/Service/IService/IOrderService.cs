b93a7eb32ba1e524f05750d7227cd4f117beab50
todor-grigorov <t.grigorov99@gmail.com>
2025-12-16 13:53:58 +0100

    feat: add/implment IOrderService and OrderService (MVC project)

---
+++ ECommerce/ECommerce.Frontend.Mvc/Service/IService/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Frontend.Mvc.Dto;
+
+namespace ECommerce.Frontend.Mvc.Service.IService
+{
+    public interface IOrderService
+    {
+        Task<ResponseDto?> CreateOrderAsync(CartDto cartDto);
+        //Task<ResponseDto?> GetCouponByIdAsync(int id);
+        //Task<ResponseDto?> GetCouponAsync(string couponCode);
+        //Task<ResponseDto?> CreateCouponAsync(CouponDto couponDto);
+        //Task<ResponseDto?> UpdateCouponAsync(CouponDto couponDto);
+        //Task<ResponseDto?> DeleteCouponAsync(int id);
+    }
+}
---
+++ ECommerce/ECommerce.Frontend.Mvc/Service/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.Frontend.Mvc.Dto;
+using ECommerce.Frontend.Mvc.Service.IService;
+using ECommerce.Frontend.Mvc.Utility;
+
+namespace ECommerce.Frontend.Mvc.Service
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IBaseService _baseService;
+
+        public OrderService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public async Task<ResponseDto?> CreateOrderAsync(CartDto cartDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = StaticDetails.ApiType.POST,
+                Data = cartDto,
+                Url = StaticDetails.OrderApiBase + "/api/order/CreateOrder"
+            });
+        }
+    }
+}
