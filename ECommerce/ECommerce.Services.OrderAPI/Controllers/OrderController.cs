f3d0a8f60b7b7b900d8fbc66fc007021fb78b5e5
todor-grigorov <t.grigorov99@gmail.com>
2025-12-16 13:44:22 +0100

    feat: add CreateOrder action method in OrderController (OrderAPI)

--- ECommerce/ECommerce.Services.OrderAPI/Controllers/OrderController.cs
+++ ECommerce/ECommerce.Services.OrderAPI/Controllers/OrderController.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
 using ECommerce.Services.OrderAPI.Data;
 using ECommerce.Services.OrderAPI.Dto;
+using ECommerce.Services.OrderAPI.Models;
 using ECommerce.Services.OrderAPI.Service.IService;
+using ECommerce.Services.OrderAPI.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.Services.OrderAPI.Controllers
@@ -21,6 +24,34 @@ namespace ECommerce.Services.OrderAPI.Controllers
             _mapper = mapper;
             _productService = productService;
             _response = new ResponseDto();
+        }
+
+
+        [Authorize]
+        [HttpPost("CreateOrder")]
+        public async Task<IActionResult> CreateOrder([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                OrderHeaderDto orderHeaderDto = _mapper.Map<OrderHeaderDto>(cartDto.CartHeader);
+                orderHeaderDto.OrderTime = DateTime.Now.ToUniversalTime();
+                orderHeaderDto.Status = StaticDetails.Status_Pending;
+                orderHeaderDto.OrderDetails = _mapper.Map<IEnumerable<OrderDetailsDto>>(cartDto.CartDetails);
+
+                OrderHeader orderCreated = _dbContext.OrderHeaders.Add(_mapper.Map<OrderHeader>(orderHeaderDto)).Entity;
+                await _dbContext.SaveChangesAsync();
+
+                orderHeaderDto.OrderHeaderId = orderCreated.OrderHeaderId;
+                _response.Result = orderHeaderDto;
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response.IsSuccess ? Ok(_response) : BadRequest(_response);
         }
     }
 }
