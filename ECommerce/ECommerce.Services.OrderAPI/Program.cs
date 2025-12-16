b2a48961533b93078bff3b477fae6bff341a1b94
todor-grigorov <t.grigorov99@gmail.com>
2025-12-16 21:05:06 +0100

    feat: implment and register StripeService (OrderAPI)

--- ECommerce/ECommerce.Services.OrderAPI/Program.cs
+++ ECommerce/ECommerce.Services.OrderAPI/Program.cs
@@ -14,12 +14,16 @@ var configuration = builder.Configuration;
 builder.Services.AddAutoMapper(cfg => { }, typeof(Program));
 builder.Services.AddDbContext<AppDbContext>(opts =>
                 opts.UseNpgsql(configuration.GetConnectionString("postgresConnection")));
+
+StaticDetails.StripeSecretKey = configuration.GetSection("StripeSettings:SecretKey").Get<string>();
+
 builder.Services.AddHttpClient("Product", c => c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"])).AddHttpMessageHandler<BackendApiAuthenticationHttpClientHandler>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<BackendApiAuthenticationHttpClientHandler>();
 builder.Services.AddScoped<IMessageBus, MessageBus>();
 builder.Services.AddScoped<IServiceBus, ServiceBus>();
+builder.Services.AddScoped<IStripeService, StripeService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ ECommerce/ECommerce.Services.OrderAPI/Service/StripeService.cs
@@ -0,0 +1,56 @@
+﻿using ECommerce.Services.OrderAPI.Dto;
+using ECommerce.Services.OrderAPI.Service.IService;
+using ECommerce.Services.OrderAPI.Utility;
+using Stripe;
+using Stripe.Checkout;
+
+namespace ECommerce.Services.OrderAPI.Service
+{
+    public class StripeService : IStripeService
+    {
+        public Session? CreateStripeSession(StripeRequestDto stripeRequestDto)
+        {
+            Session session;
+            try
+            {
+                StripeConfiguration.ApiKey = StaticDetails.StripeSecretKey;
+
+                var options = new Stripe.Checkout.SessionCreateOptions
+                {
+                    SuccessUrl = stripeRequestDto.ApprovedUrl,
+                    CancelUrl = stripeRequestDto.CancelUrl,
+                    LineItems = new List<SessionLineItemOptions>(),
+                    Mode = "payment",
+                };
+
+                foreach (var item in stripeRequestDto.OrderHeader.OrderDetails)
+                {
+                    var lineItem = new SessionLineItemOptions
+                    {
+                        PriceData = new SessionLineItemPriceDataOptions
+                        {
+                            UnitAmount = (long)(item.Price * 100), // $20.99 -> 2099 cents
+                            Currency = "usd",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = item.ProductName,
+                            },
+                        },
+                        Quantity = item.Count,
+                    };
+                    options.LineItems.Add(lineItem);
+                }
+
+
+                var service = new SessionService();
+                session = service.Create(options);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return session;
+        }
+    }
+}
