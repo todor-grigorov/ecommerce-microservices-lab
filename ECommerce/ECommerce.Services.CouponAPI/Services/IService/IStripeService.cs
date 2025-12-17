96c4f51af569e201d82a95a18b09c92659e0802c
todor-grigorov <t.grigorov99@gmail.com>
2025-12-17 17:22:29 +0100

    feat: add IStripeService with CreateCoupon and DeleteCoupon actions

---
+++ ECommerce/ECommerce.Services.CouponAPI/Services/IService/IStripeService.cs
@@ -0,0 +1,10 @@
+﻿using Stripe;
+
+namespace ECommerce.Services.CouponAPI.Services.IService
+{
+    public interface IStripeService
+    {
+        Task<Coupon?> CreateCoupon(long amountOff, string name, string currency, string id);
+        Task<bool> DeleteCoupon(string id);
+    }
+}
