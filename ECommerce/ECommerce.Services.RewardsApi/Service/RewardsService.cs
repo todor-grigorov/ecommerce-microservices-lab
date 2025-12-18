d52f18792115492ccd22af86b153555dae27d5dd
todor-grigorov <t.grigorov99@gmail.com>
2025-12-19 00:08:04 +0100

    feat: implement RewardsService and UpdateRewards method (Rewards Api)

---
+++ ECommerce/ECommerce.Services.RewardsApi/Service/RewardsService.cs
@@ -0,0 +1,44 @@
+﻿using ECommerce.Services.EmailAPI.Service.IService;
+using ECommerce.Services.RewardsApi.Data;
+using ECommerce.Services.RewardsApi.Dto;
+using ECommerce.Services.RewardsApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Services.EmailAPI.Service
+{
+    public class RewardsService : IRewardsService
+    {
+        private DbContextOptions<AppDbContext> _dbOptions;
+
+        public RewardsService(DbContextOptions<AppDbContext> dbOptions)
+        {
+            _dbOptions = dbOptions;
+        }
+
+        public async Task UpdateRewards(RewardsMessageDto rewardsMessageDto)
+        {
+            try
+            {
+                Rewards rewards = new Rewards()
+                {
+                    OrderId = rewardsMessageDto.OrderId,
+                    RewardsActivity = rewardsMessageDto.RewardsActivity,
+                    UserId = rewardsMessageDto.UserId,
+                    RewardsDate = DateTime.Now.ToUniversalTime(),
+                };
+
+
+
+                await using var db = new AppDbContext(_dbOptions);
+                await db.Rewards.AddAsync(rewards);
+                await db.SaveChangesAsync();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error updating rewards: {ex.Message}");
+            }
+        }
+    }
+}
