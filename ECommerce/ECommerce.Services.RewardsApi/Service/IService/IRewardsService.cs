35f6da5dbe5f6eb0b1bb8cb1351bb92db128f6d0
todor-grigorov <t.grigorov99@gmail.com>
2025-12-19 00:07:46 +0100

    feat: create  IRewardsService with UpdateRewards method (Rewards Api)

---
+++ ECommerce/ECommerce.Services.RewardsApi/Service/IService/IRewardsService.cs
@@ -0,0 +1,9 @@
+﻿using ECommerce.Services.RewardsApi.Dto;
+
+namespace ECommerce.Services.EmailAPI.Service.IService
+{
+    public interface IRewardsService
+    {
+        Task UpdateRewards(RewardsMessageDto rewardsMessageDto);
+    }
+}
