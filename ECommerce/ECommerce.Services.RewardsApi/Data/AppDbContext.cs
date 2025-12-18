9eabc51dbe256bc2659d388f847e73215c073f64
todor-grigorov <t.grigorov99@gmail.com>
2025-12-18 20:40:28 +0100

    feat: add RewardsApi db context

---
+++ ECommerce/ECommerce.Services.RewardsApi/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Services.RewardsApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Services.RewardsApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Rewards> Rewards { get; set; }
+
+    }
+}
