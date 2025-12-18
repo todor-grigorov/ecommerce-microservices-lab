ceb31cff1ada6335646907151bc26a58c756e6e9
todor-grigorov <t.grigorov99@gmail.com>
2025-12-18 20:41:30 +0100

    feat: add Rewards DTO (OrderAPI)

---
+++ ECommerce/ECommerce.Services.OrderAPI/Dto/RewardsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerce.Services.OrderAPI.Dto
+{
+    public record RewardsDto
+    {
+        public string UserId { get; set; }
+        public int RewardsActivity { get; set; }
+        public int OrderId { get; set; }
+    };
+}
