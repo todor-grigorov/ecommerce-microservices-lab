00b1a0e01b030b539e829f8a79b4d96b118d406a
todor-grigorov <t.grigorov99@gmail.com>
2025-12-22 14:01:36 +0100

    feat: add RewardsMessageDto to EmailAPI project

---
+++ ECommerce/ECommerce.Services.EmailAPI/Dto/RewardsMessageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerce.Services.EmailAPI.Dto
+{
+    public record RewardsMessageDto
+    {
+        public string UserId { get; set; }
+        public int RewardsActivity { get; set; }
+        public int OrderId { get; set; }
+    }
+}
