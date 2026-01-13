297b206a081ea0aa901aed6b469c5de642623696
todor-grigorov <t.grigorov99@gmail.com>
2026-01-13 19:41:01 +0100

    feat: implement GetAllOrders, GetOrder and UpdateOrderStatus in OrderService (MVC project)

--- ECommerce/ECommerce.Frontend.Mvc/Service/OrderService.cs
+++ ECommerce/ECommerce.Frontend.Mvc/Service/OrderService.cs
@@ -33,6 +33,34 @@ namespace ECommerce.Frontend.Mvc.Service
             });
         }
 
+        public async Task<ResponseDto?> GetAllOrders(string? userId)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = StaticDetails.ApiType.GET,
+                Url = StaticDetails.OrderApiBase + "/api/order/GetOrders" + userId,
+            });
+        }
+
+        public async Task<ResponseDto?> GetOrder(int orderId)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = StaticDetails.ApiType.GET,
+                Url = StaticDetails.OrderApiBase + "/api/order/GetOrder",
+            });
+        }
+
+        public async Task<ResponseDto?> UpdateOrderStatus(int orderId, string status)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = StaticDetails.ApiType.POST,
+                Data = status,
+                Url = StaticDetails.OrderApiBase + "/api/order/UpdateOrderStatus" + orderId,
+            });
+        }
+
         public async Task<ResponseDto?> ValidateStripeSession(int orderHeaderId)
         {
             return await _baseService.SendAsync(new RequestDto()
