fbaaa6cc87b49feaafaa3e3d2c0a191685d452bb
todor-grigorov <t.grigorov99@gmail.com>
2026-01-13 20:35:48 +0100

    feat: add OrderController with GetAll action in (MVC project)

---
+++ ECommerce/ECommerce.Frontend.Mvc/Controllers/OrderController.cs
@@ -0,0 +1,44 @@
+﻿using ECommerce.Frontend.Mvc.Dto;
+using ECommerce.Frontend.Mvc.Service.IService;
+using ECommerce.Frontend.Mvc.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace ECommerce.Frontend.Mvc.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _orderService;
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public IActionResult OrderIndex()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            IEnumerable<OrderHeaderDto>? list = new List<OrderHeaderDto>();
+            string? userId = "";
+
+            if (!User.IsInRole(StaticDetails.RoleAdmin))
+            {
+                userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+            }
+
+            ResponseDto? response = _orderService.GetAllOrders(userId).GetAwaiter().GetResult();
+
+            if (response != null && response.IsSuccess && response.Result != null)
+            {
+                list = JsonConvert.DeserializeObject<List<OrderHeaderDto>>(Convert.ToString(response.Result));
+            }
+
+            return Json(new { data = list });
+        }
+    }
+}
