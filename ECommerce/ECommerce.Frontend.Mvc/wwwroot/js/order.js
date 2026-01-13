0470bd38d50115b1a6c0228e9a2b6e00b469a588
todor-grigorov <t.grigorov99@gmail.com>
2026-01-13 20:36:53 +0100

    feat: add order.js to load data for the orders table

---
+++ ECommerce/ECommerce.Frontend.Mvc/wwwroot/js/order.js
@@ -0,0 +1,26 @@
+﻿var dataTable;
+
+$(document).ready(function () { loadDataTable(); });
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: "/order/getall" },
+        "columns": [
+            { data: 'orderHeaderId', "width": "5%" },
+            { data: 'email', "width": "25%" },
+            { data: "name", width: "20%" },           // Customer Name
+            { data: "phone", width: "15%" },    // Customer Phone
+            { data: "status", width: "10%" },    // Status
+            { data: "orderTotal", width: "10%" },
+            {                                         // Actions (blank header)
+                data: "orderHeaderId",
+                width: "10%",
+                orderable: false,
+                searchable: false,
+                render: function (data) {
+                    return `<div class="w-75 btn-group" role="group"><a class="btn btn-sm btn-primary mx-2" href="/order/details?id=${data}"><i class="bi bi-pencil-square"></i></a></div>`;
+                }
+            }
+        ]
+    })
+}
