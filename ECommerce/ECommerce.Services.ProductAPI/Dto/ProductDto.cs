60eb8fda857e1e5b76ddba644e9a867ebd5d9a17
todor-grigorov <t.grigorov99@gmail.com>
2026-01-15 17:55:32 +0100

    feat: add ImageLocalPath to all Product models and DTOs

--- ECommerce/ECommerce.Services.ProductAPI/Dto/ProductDto.cs
+++ ECommerce/ECommerce.Services.ProductAPI/Dto/ProductDto.cs
@@ -7,6 +7,7 @@
         public double Price { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
-        public string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? ImageLocalPath { get; set; }
     }
 }
