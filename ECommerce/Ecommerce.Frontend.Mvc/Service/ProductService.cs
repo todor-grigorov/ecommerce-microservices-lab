7cfb50e789fe774b2d7d8f4e39769db5664799ee
todor-grigorov <t.grigorov99@gmail.com>
2026-01-15 21:21:17 +0100

    feat: add ContentType.MultipartFormData to create and update methods in ProductService (MVC project)

--- ECommerce/Ecommerce.Frontend.Mvc/Service/ProductService.cs
+++ ECommerce/Ecommerce.Frontend.Mvc/Service/ProductService.cs
@@ -19,7 +19,8 @@ namespace ECommerce.Frontend.Mvc.Service
             {
                 ApiType = StaticDetails.ApiType.POST,
                 Data = productDto,
-                Url = StaticDetails.ProductApiBase + "/api/product"
+                Url = StaticDetails.ProductApiBase + "/api/product",
+                ContentType = StaticDetails.ContentType.MultipartFormData
             });
         }
 
@@ -56,7 +57,8 @@ namespace ECommerce.Frontend.Mvc.Service
             {
                 ApiType = StaticDetails.ApiType.PUT,
                 Data = productDto,
-                Url = StaticDetails.ProductApiBase + "/api/product"
+                Url = StaticDetails.ProductApiBase + "/api/product",
+                ContentType = StaticDetails.ContentType.MultipartFormData
             });
         }
     }
