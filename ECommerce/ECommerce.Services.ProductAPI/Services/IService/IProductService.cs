925dc80a171147d8c390a540a89d0293fd432afa
todor-grigorov <t.grigorov99@gmail.com>
2026-01-16 13:25:12 +0100

    feat: add IProductService and ProductService with methods for crating and deleting product image

---
+++ ECommerce/ECommerce.Services.ProductAPI/Services/IService/IProductService.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.Services.ProductAPI.Services.IService
+{
+    public interface IProductService
+    {
+        (string ImageUrl, string ImageLocalPath) CreateProductImage(IFormFile productImageFile, int productId, string baseUrl);
+        void DeleteProductImage(string productImageLocalPath);
+    }
+}
---
+++ ECommerce/ECommerce.Services.ProductAPI/Services/ProductService.cs
@@ -0,0 +1,35 @@
+﻿using ECommerce.Services.ProductAPI.Services.IService;
+
+namespace ECommerce.Services.ProductAPI.Services
+{
+    public class ProductService : IProductService
+    {
+        public (string ImageUrl, string ImageLocalPath) CreateProductImage(IFormFile productImageFile, int productId, string baseUrl)
+        {
+            string fileName = productId + Path.GetExtension(productImageFile.FileName);
+            string filePath = @"wwwroot\ProductImages\" + fileName;
+            var filePathDirectory = Path.Combine(Directory.GetCurrentDirectory(), filePath);
+
+            using (var stream = new FileStream(filePathDirectory, FileMode.Create))
+            {
+                productImageFile.CopyTo(stream);
+            }
+
+            var imageUrl = baseUrl + "/ProductImages/" + fileName;
+            var imageLocalPath = filePath;
+
+            return (imageUrl, imageLocalPath);
+        }
+
+        public void DeleteProductImage(string productImageLocalPath)
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), productImageLocalPath);
+            FileInfo file = new FileInfo(filePath);
+
+            if (file.Exists)
+            {
+                file.Delete();
+            }
+        }
+    }
+}
