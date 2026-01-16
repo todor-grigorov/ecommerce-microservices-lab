22e9e4784c69a85fa6ffe09b46b3c34aaed713c5
todor-grigorov <t.grigorov99@gmail.com>
2026-01-16 15:57:53 +0100

    feat: add authentication support to Gateway API

---
+++ ECommerce/ECommerce.Gateway.Api/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace ECommerce.Gateway.Api.Extensions
+{
+    public static class WebApplicationBuilderExtensions
+    {
+        public static WebApplicationBuilder AddAppAuthentication(this WebApplicationBuilder builder)
+        {
+            var settingsSection = builder.Configuration.GetSection("ApiSettings");
+
+            var sercret = settingsSection.GetValue<string>("SecretKey");
+            var issuer = settingsSection.GetValue<string>("Issuer");
+            var audience = settingsSection.GetValue<string>("Audience");
+
+            var key = Encoding.ASCII.GetBytes(sercret);
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = issuer,
+                    ValidAudience = audience
+                };
+            });
+
+            return builder;
+        }
+    }
+}
