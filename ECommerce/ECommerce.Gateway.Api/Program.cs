28e5c6ed1928c6bfbdb3d2129b0d5b7c8059f49a
todor-grigorov <t.grigorov99@gmail.com>
2026-01-16 14:29:59 +0100

    feat: register Ocelot in Program.cs (Gateway)

--- ECommerce/ECommerce.Gateway.Api/Program.cs
+++ ECommerce/ECommerce.Gateway.Api/Program.cs
@@ -1,6 +1,12 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+builder.Services.AddOcelot();
+
 app.MapGet("/", () => "Hello World!");
+app.UseOcelot();
 
 app.Run();
