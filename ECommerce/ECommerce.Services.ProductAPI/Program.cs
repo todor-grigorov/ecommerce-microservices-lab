769b586f075698aa0374308b5866a68a08472f76
todor-grigorov <t.grigorov99@gmail.com>
2026-01-18 17:05:15 +0100

    feat: configure swagger ui for production (ProductAPI)

--- ECommerce/ECommerce.Services.ProductAPI/Program.cs
+++ ECommerce/ECommerce.Services.ProductAPI/Program.cs
@@ -24,11 +24,12 @@ builder.AddAppAuthentication();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ECommerce.Services.ProductAPI v1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
