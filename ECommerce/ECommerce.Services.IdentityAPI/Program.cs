aa7d82b1a7852bdcdf4043b438dfe7be26728fa9
todor-grigorov <t.grigorov99@gmail.com>
2026-01-18 16:05:38 +0100

    refactor: desplay swagger UI on production

--- ECommerce/ECommerce.Services.IdentityAPI/Program.cs
+++ ECommerce/ECommerce.Services.IdentityAPI/Program.cs
@@ -32,11 +32,12 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ECommerce.Services.IdentityAPI v1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
