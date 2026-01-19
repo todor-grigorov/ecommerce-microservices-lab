72f99cdc12b440a12560c672d7cf5c4600bf1ed7
todor-grigorov <t.grigorov99@gmail.com>
2026-01-19 16:27:07 +0100

    refator: add swagger ui for production ShoppingCartAPI

--- ECommerce/ECommerce.Services.ShoppingCartAPI/Program.cs
+++ ECommerce/ECommerce.Services.ShoppingCartAPI/Program.cs
@@ -48,11 +48,12 @@ builder.AddAppAuthentication();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ECommerce.Services.ShoppingCartAPI v1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
