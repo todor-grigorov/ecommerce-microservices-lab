28a0e05fe4ac4f5bae698c2ae16442a1c8de2df4
todor-grigorov <t.grigorov99@gmail.com>
2026-01-19 16:36:11 +0100

    refactor: add production swagger ui for OrderAPI

--- ECommerce/ECommerce.Services.OrderAPI/Program.cs
+++ ECommerce/ECommerce.Services.OrderAPI/Program.cs
@@ -35,11 +35,12 @@ builder.AddAppAuthentication();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ECommerce.Services.OrderAPI v1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
