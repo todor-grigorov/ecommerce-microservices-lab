0c59f73bf72567b0ed15006cef49bf0798fe9f5a
todor-grigorov <t.grigorov99@gmail.com>
2026-01-20 16:28:42 +0100

    refactor: Product entities add to the DB in OnModelCreating (ProductAPI)

--- ECommerce/ECommerce.Services.ProductAPI/Data/AppDbContext.cs
+++ ECommerce/ECommerce.Services.ProductAPI/Data/AppDbContext.cs
@@ -17,38 +17,38 @@ namespace ECommerce.Services.ProdictAPI.Data
             modelBuilder.Entity<Product>().HasData(new Product
             {
                 ProductId = 1,
-                Name = "Samosa",
-                Price = 15,
-                Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                Name = "Shopska Salad",
+                Price = 11,
+                Description = "A refreshing mix of chopped tomatoes, cucumbers, onions, peppers, and grated sirene (white brine cheese), named after the Shops region.",
                 ImageUrl = "https://placehold.co/603x403",
                 CategoryName = "Appetizer"
             });
             modelBuilder.Entity<Product>().HasData(new Product
             {
                 ProductId = 2,
-                Name = "Paneer Tikka",
-                Price = 13.99,
-                Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                Name = "Snezhanka salad",
+                Price = 9.99,
+                Description = "Traditional Bulgarian salad, which is made of strained yogurt, cucumber, garlic, salt, usually cooking oil, dill, sometimes roasted peppers, walnuts and parsley.",
                 ImageUrl = "https://placehold.co/602x402",
                 CategoryName = "Appetizer"
             });
             modelBuilder.Entity<Product>().HasData(new Product
             {
                 ProductId = 3,
-                Name = "Sweet Pie",
+                Name = "Tikvenik",
                 Price = 10.99,
-                Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                Description = "Pastry consisting of thin sheets of dough that are filled with grated pumpkin, coarsely ground walnuts, sugar, and cinnamon.",
                 ImageUrl = "https://placehold.co/601x401",
                 CategoryName = "Dessert"
             });
             modelBuilder.Entity<Product>().HasData(new Product
             {
                 ProductId = 4,
-                Name = "Pav Bhaji",
+                Name = "Kavarma ",
                 Price = 15,
-                Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                Description = "Kavarma is a hearty, slow-cooked stew from Bulgarian cuisine, typically made with chunks of pork, chicken, or beef, simmered with vegetables like onions, peppers, tomatoes, and mushrooms, often in a traditional clay pot (gyuveche) with spices like paprika and savory (chubritsa), resulting in a rich, flavorful main course, sometimes finished with an egg.",
                 ImageUrl = "https://placehold.co/600x400",
-                CategoryName = "Entree"
+                CategoryName = "Main"
             });
         }
     }
