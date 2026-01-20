88b2286aeedcfa29359f971bd4b8a892f2ac6344
todor-grigorov <t.grigorov99@gmail.com>
2026-01-20 19:32:06 +0100

    feat: configure Ocelot's to use Azure App Configuration resource for Routes and GlobalConfiguration configuration

--- ECommerce/ECommerce.Gateway.Api/Program.cs
+++ ECommerce/ECommerce.Gateway.Api/Program.cs
@@ -4,6 +4,20 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var appConfigConn = builder.Configuration["AppConfigConnectionString"];
+
+if (!builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddAzureAppConfiguration(options =>
+    {
+        options.Connect(appConfigConn)
+               // If labels per environment are used, I need to pick one:
+               // .Select(KeyFilter.Any, builder.Environment.EnvironmentName)
+               .Select("Routes", builder.Environment.EnvironmentName)
+               .Select("GlobalConfiguration", builder.Environment.EnvironmentName);
+    });
+}
+
 builder.AddAppAuthentication();
 builder.Services.AddOcelot();
 
